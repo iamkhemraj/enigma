31fc069c9e3ba7bc210c0472ee2f8307f39458c3
Khemraj <khemraj@gmail.com>
2024-03-04 12:49:41 +0530

    craete archive file and add woo commerce theme supports

--- functions.php
+++ functions.php
@@ -4,4 +4,13 @@
   }
   require_once 'libs/helper.php';
   
+
+  // add theme sport
+add_action( 'after_setup_theme', 'setup_woocommerce_support' );
+
+function setup_woocommerce_support()
+{
+add_theme_support('woocommerce');
+}
+
 ?>
