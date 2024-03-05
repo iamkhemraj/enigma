88613fc6497725d9b0b50a479a4842a322200a2e
Khemraj <khemraj@gmail.com>
2024-03-05 13:55:01 +0530

    add logical condition

--- libs/helper.php
+++ libs/helper.php
@@ -1,46 +1,52 @@
 <?php
-
+if ( !defined('ABSPATH') ) {
+  exit;
+}
+// WooCommerce helper file.
+require 'woocommerce.php';
 
 // Enigma theme style script here
+if ( !function_exists('engima_wp_enqueue_scripts') ) {
+  add_action( 'wp_enqueue_scripts', 'engima_wp_enqueue_scripts' );
+  function  engima_wp_enqueue_scripts() {
+    wp_enqueue_style('main-style', get_template_directory_uri().'/assets/styles/css/app.css' );
+    wp_enqueue_style('main-splide-min', get_template_directory_uri().'/assets/styles/css/splide.min.css' );
+    wp_enqueue_style('main-all', get_template_directory_uri().'/assets/styles/css/all.css' );
+    wp_enqueue_style('main-bootstrap', get_template_directory_uri().'/assets/styles/css/bootstrap.min.css' );
+    // Start script
 
-function  engima_wp_enqueue_scripts() {
-  wp_enqueue_style('main-style', get_template_directory_uri().'/assets/styles/css/app.css' );
-  wp_enqueue_style('main-splide-min', get_template_directory_uri().'/assets/styles/css/splide.min.css' );
-  wp_enqueue_style('main-all', get_template_directory_uri().'/assets/styles/css/all.css' );
-  wp_enqueue_style('main-bootstrap', get_template_directory_uri().'/assets/styles/css/bootstrap.min.css' );
-  // Start script
-
-  wp_enqueue_script('main-script', get_template_directory_uri() . '/assets/js/splide.min.js' );
-  wp_enqueue_script('main-jquery-min', get_template_directory_uri() . '/assets/js/jquery.min.js' );
-  wp_enqueue_script('main-main', get_template_directory_uri() . '/assets/js/main.js' );
+    wp_enqueue_script('main-script', get_template_directory_uri() . '/assets/js/splide.min.js' );
+    wp_enqueue_script('main-jquery-min', get_template_directory_uri() . '/assets/js/jquery.min.js' );
+    wp_enqueue_script('main-main', get_template_directory_uri() . '/assets/js/main.js' );
+  }
 }
-add_action( 'wp_enqueue_scripts', 'engima_wp_enqueue_scripts' );
-
 
 // Theme custom logo start here 
-function enigma_custom_logo_setup() {
-  $defaults = array(
-    'height'               => 10,
-    'width'                => 100,
-    'flex-height'          => true,
-    'flex-width'           => true,
-    'header-text'          => array( 'site-title', 'site-description' ),
-    'unlink-homepage-logo' => true, 
-  );
-  add_theme_support( 'custom-logo', $defaults );
+if ( !function_exists('enigma_custom_logo_setup')) {
+  add_action( 'after_setup_theme', 'enigma_custom_logo_setup' );
+  function enigma_custom_logo_setup() {
+    $defaults = array(
+      'height'               => 10,
+      'width'                => 100,
+      'flex-height'          => true,
+      'flex-width'           => true,
+      'header-text'          => array( 'site-title', 'site-description' ),
+      'unlink-homepage-logo' => true, 
+    );
+    add_theme_support( 'custom-logo', $defaults );
+  }
 }
-add_action( 'after_setup_theme', 'enigma_custom_logo_setup' );
-
-// Theme custom logo close.
 
 // Enigma wp theme nav menu.
-function register_my_menus() {
-  register_nav_menus(
-      array(
-		'primary_menu' => __('Primary Menu'),
-		// Add more menu locations if needed
-      )
-  );
+if ( !function_exists('enigma_custom_logo_setup')) {
+  add_action('init', 'register_my_menus');
+  function register_my_menus() {
+    register_nav_menus(
+        array(
+      'primary_menu' => __('Primary Menu'),
+      // Add more menu locations if needed
+        )
+    );
+  }
 }
-add_action('init', 'register_my_menus');
 ?>
---
+++ libs/woocommerce.php
@@ -0,0 +1,58 @@
+<?php
+if ( !defined('ABSPATH') ) {
+  exit;
+}
+add_theme_support( 'woocommerce' );
+add_theme_support( 'wc-product-gallery-slider' );
+
+// Remove default wrapper class
+remove_action( 'woocommerce_before_main_content', 'woocommerce_output_content_wrapper', 10);
+remove_action( 'woocommerce_after_main_content', 'woocommerce_output_content_wrapper_end', 10);
+
+// Remove breadcrumb
+remove_action( 'woocommerce_before_main_content', 'woocommerce_breadcrumb', 20 );
+
+// Adding custom woocommerce wrapper
+if ( !function_exists('woocommerce_container_start_class') ) {
+  add_action('woocommerce_before_main_content', 'woocommerce_container_start_class', 10);
+  function woocommerce_container_start_class() {
+    echo '
+    <div id="main-woocommerce" class="woocomerce">
+      <div class="woocomerce__container">
+        <div class="woocomerce__container__content grid">
+    ';
+  }
+}
+
+if ( !function_exists('woocommerce_container_start_class') ) {
+  add_action('woocommerce_after_main_content', 'woocommerce_container_end_class', 10);
+  function woocommerce_container_end_class() {
+    echo '
+        </div>
+      </div>
+    </div>';
+  }
+}
+
+if ( !function_exists('woocommerce_image_support') ) {
+  add_action( 'after_setup_theme', 'woocommerce_image_support', 10 );
+  function woocommerce_image_support() {
+    add_theme_support( 'woocommerce', array(
+        'thumbnail_image_width' => 150,
+        'single_image_width'    => 150,
+
+        'product_grid'          => array(
+            'default_rows'    => 3,
+            'min_rows'        => 2,
+            'max_rows'        => 8,
+            'default_columns' => 4,
+            'min_columns'     => 2,
+            'max_columns'     => 5,
+        ),
+    ) );
+  }
+}
+
+
+
+	
