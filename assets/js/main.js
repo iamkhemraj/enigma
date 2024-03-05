4fec238a2736203a85a74a717a872b34ff159a26
Khemraj <khemraj@gmail.com>
2024-03-05 09:51:19 +0530

    add images

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,35 +1,4 @@
-  alert('hi');
-  document.addEventListener('DOMContentLoaded', function () {
-    var main = new Splide('#main-slider', {
-      type: 'fade',
-      heightRatio: 0.5,
-      pagination: false,
-      arrows: false,
-      cover: true,
+ document.addEventListener('DOMContentLoaded', function () {
+        var splide = new Splide('.splide');
+        splide.mount();
     });
-
-    var thumbnails = new Splide('#thumbnail-slider', {
-      rewind: true,
-      fixedWidth: 104,
-      fixedHeight: 58,
-      isNavigation: true,
-      gap: 10,
-      focus: 'center',
-      pagination: false,
-      cover: true,
-      dragMinThreshold: {
-        mouse: 4,
-        touch: 10,
-      },
-      breakpoints: {
-        640: {
-          fixedWidth: 66,
-          fixedHeight: 38,
-        },
-      },
-    });
-
-    main.sync(thumbnails);
-    main.mount();
-    thumbnails.mount();
-  });
--- home.php
+++ home.php
@@ -1,41 +1,16 @@
 <?php get_header(); ?>
 
+<div class="splide">
+    <div class="splide__track">
+        <ul class="splide__list">
+            <li class="splide__slide"><img src="<?= get_template_directory_uri().'/assets/img/istockphoto-1347632685-170667a.webp'?>"></li>  
 
-
-
-<div id="main-slider" class="splide">
-  <div class="splide__track">
-    <ul class="splide__list">
-      <!-- Add your main slider content here -->
-      <li class="splide__slide">
-        <img src="<?= get_template_directory_uri().'/assets/img/01.jpg'?>" alt="">
-      </li>
-      <li class="splide__slide">
-        <img src="<?= get_template_directory_uri().'/assets/img/download (1).jpg'?>" alt="">
-      </li>
-   
-    </ul>
-  </div>
+            <li class="splide__slide"><img src="<?= get_template_directory_uri().'/assets/img/istockphoto-863190232-170667a.jpg'?>"></li>
+            <li class="splide__slide"><img src="<?= get_template_directory_uri().'/assets/img/istockphoto-1073935306-170667.jpg'?>"></li>
+            
+        </ul>
+    </div>
 </div>
-
-<div id="thumbnail-slider" class="splide">
-  <div class="splide__track">
-    <ul class="splide__list">
-      <!-- Add your thumbnail slider content here -->
-      <li class="splide__slide">
-        <img src="<?= get_template_directory_uri().'/assets/img/01.jpg'?>" alt="">
-      </li>
-      <li class="splide__slide">
-        <img src="<?= get_template_directory_uri().'/assets/img/download (1).jpg'?>" alt="">
-      </li>
-     
-    </ul>
-  </div>
-</div>
-
-<!-- <script src="https://unpkg.com/@splidejs/splide@3.0.7/dist/js/splide.min.js"></script> -->
-
-
 
 
 <?php get_footer(); ?>
