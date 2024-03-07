1779a72715df3f311117b8f302d9bc321b1ae974
Khemraj <khemraj@gmail.com>
2024-03-07 18:00:20 +0530

    add navbar update

--- header.php
+++ header.php
@@ -1,79 +1,83 @@
 
-<?php
-if ( ! defined( 'ABSPATH' ) ) {
-  exit; // Terminate script execution
-}
-?>
-<!DOCTYPE html>
-<html <?php language_attributes(); ?>>
+	<?php
+	if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Terminate script execution
+	}
+	?>
+	<!DOCTYPE html>
+	<html <?php language_attributes(); ?>>
 
-<head>
-  <meta charset="<?php bloginfo('charset'); ?>" />
-  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-  <script src="
-https://cdn.jsdelivr.net/npm/@splidejs/splide@4.1.4/dist/js/splide.min.js
-"></script>
-<link href="
-https://cdn.jsdelivr.net/npm/@splidejs/splide@4.1.4/dist/css/splide.min.css
-" rel="stylesheet">
-  <title>
-    <?php
-     if( is_404() ){
-      echo 'Page Not Found - '.get_bloginfo('name');
-     } elseif (is_front_page()) {
-      echo get_bloginfo('name');
-     } else{
-      echo get_the_title() . ' - ' . get_bloginfo('name');
-     }
-    ?>
-  </title>
-  <?php wp_head(); ?>
-</head>
-<body <?php body_class(); ?>>
+	<head>
+	<meta charset="<?php bloginfo('charset'); ?>" />
+	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+	<script src="
+	https://cdn.jsdelivr.net/npm/@splidejs/splide@4.1.4/dist/js/splide.min.js
+	"></script>
+	<link href="
+	https://cdn.jsdelivr.net/npm/@splidejs/splide@4.1.4/dist/css/splide.min.css
+	" rel="stylesheet">
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+	<title>
+		<?php
+		if( is_404() ){
+		echo 'Page Not Found - '.get_bloginfo('name');
+		} elseif (is_front_page()) {
+		echo get_bloginfo('name');
+		} else{
+		echo get_the_title() . ' - ' . get_bloginfo('name');
+		}
+		?>
+	</title>
+	<?php wp_head(); ?>
+	
+	</head>
+	<body <?php body_class(); ?>>
 
-<nav class="navigation">
-  <div class="container">
-      <div class="row">
-          <div class="col-md-12">
-              <div class="block">
-                  <nav class="navbar ">
-                    <div class="container-fluid">
-                      <!-- Brand and toggle get grouped for better mobile display -->
-                      <div class=" navbar-header">
-                        <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
-                          <span class="sr-only">Toggle navigation</span>
-                          <span class="icon-bar"></span>
-                          <span class="icon-bar"></span>
-                          <span class="icon-bar"></span>
-                        </button>
-                          <a class="navbar-brand" href="#">
-                            <?php
-                              $custom_logo_id = get_theme_mod( 'custom_logo' );
-                              $logo = wp_get_attachment_image_src( $custom_logo_id , 'full' );
-                              if ( has_custom_logo() ) {
-                                echo '<img src="' . esc_url( $logo[0] ) . '" alt="' . get_bloginfo( 'name' ) . '" class="logo_img" >';
-                              } else {
-                                echo '<h1>' . get_bloginfo('name') . '</h1>';
-                              }
-                            ?>
-                          </a>
+	<nav class="nav" style="background-color: #DED6C0;">
+		<div class="nav__container  container ">
+			<nav class=" nav__navbar row" >
+				<div class="col-md-4">				
+					<a class="nav__logo navbar-brand " href="#">
+						<?php
+							$custom_logo_id = get_theme_mod('custom_logo');
+							$logo = wp_get_attachment_image_src($custom_logo_id, 'full');
+							if (has_custom_logo()) {
+							echo '<img src="' . esc_url($logo[0]) . '" alt="' . get_bloginfo('name') . '" class="logo_img">';
+							} else {
+							echo '<h1>' . get_bloginfo('name') . '</h1>';
+							}
+						?>
+					</a>
+				</div>
+				<div class="col-md-4" style="margin: 50px 0px 0px 0px " >
+					<?php get_search_form(); ?>
+				</div>
+				<div class="col-md-4 " style="margin: 50px 0px 0px 0px " >
+					<?php do_shortcode('xoo_wsc_cart'); ?>
+				</div>`
+			</nav>
+			<div class="row">
+				<div class="col-md-12">
+					<nav class="navbar navbar-expand-sm " style="margin-left:322px;">
+						<div class="container">
+							<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#collapsibleNavbar">
+							<span class="navbar-toggler-icon"></span>
+							</button>
+							<div class="collapse navbar-collapse" id="collapsibleNavbar">
+								<?php
+									wp_nav_menu(array(
+									'container'  => 'ul',
+									'menu_class' => 'nav navbar-nav navbar-center',
+									));
+								?>
 
-                      </div>
-
-                      <!-- Collect the nav links, forms, and other content for toggling -->
-                      <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-                      
-                        <?php
-                          wp_nav_menu(array(
-                            'container'  => 'ul',
-                            'menu_class' =>  ' nav navbar-nav navbar-right ',
-                            ));
-                        ?>
-                      </div><!-- /.navbar-collapse -->
-                    </div><!-- /.container-fluid -->
-                  </nav>
-              </div>
-          </div><!-- .col-md-12 close -->
-      </div><!-- .row close -->
-  </div><!-- .container close -->
-</nav><!-- header close -->
+							</div>
+						</div>
+					</nav>
+				
+				</div>
+			</div>
+		</div>
+	</nav>
+	<!-- header close -->
