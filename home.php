8bd442ce7f819622d3dd31e5b88e5e527877adb9
Khemraj <khemraj@gmail.com>
2024-03-07 18:00:30 +0530

    update

--- home.php
+++ home.php
@@ -20,18 +20,24 @@
     <!-- Main section close -->
     <section id="collection" class="collection">
         <div class="container">
-            <h2 class="section_title">New Collection</h2>
+            <h2 class="section__title text-center p-2">New Collection</h2>
             <div class="collection__content" id="collection__content">
-                
-                   <div class="container" style="background-color: rgb(255, 255, 255); padding: 16px;">
+                <div class="container" style="background-color: rgb(255, 255, 255); padding: 16px;">
                    <div class="row">
                        <div class="col-md-12">
-                           <?php echo do_shortcode('[products limit="3" columns="3"]'); ?>
+                            <div class="product__content">
+                                <?php echo do_shortcode('[products limit="6" columns="6"]'); ?>
+                            </div>
+                            <div class="product__content">
+                                <?php echo do_shortcode('[products limit="6" columns="6"  order="desc"]'); ?>
+                            </div>
                        </div>
                    </div>
                </div> 
-              
-
             </div>
         </div>
+    </section>
+    <section class ="section-three">
+        <img src="" alt="">
+    </section>
 <?php get_footer(); ?>
