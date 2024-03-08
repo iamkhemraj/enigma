9d6e4e7da17aa9268737cac8b279f3106837206b
Khemraj <khemraj@gmail.com>
2024-03-08 17:13:58 +0530

    Formatting code only this file

--- header.php
+++ header.php
@@ -9,7 +9,7 @@ if (!defined('ABSPATH')) {
 <head>
     <meta charset="<?php bloginfo('charset'); ?>" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    
+
     <title>
         <?php
         if (is_404()) {
@@ -32,16 +32,14 @@ if (!defined('ABSPATH')) {
             <div class=" nav__row row align-items-center">
                 <div class="nav__content col-md-3">
                     <!-- Logo -->
-                    <a class="nav__content__logo" href="#">
-                        <?php
+                    <a class="nav__content__logo" href="<?= url('/')?>"> <?php
                         $custom_logo_id = get_theme_mod('custom_logo');
                         $logo = wp_get_attachment_image_src($custom_logo_id, 'full');
                         if (has_custom_logo()) {
                             echo '<img src="' . esc_url($logo[0]) . '" alt="' . get_bloginfo('name') . '" class="logo_img">';
                         } else {
                             echo '<h1>' . get_bloginfo('name') . '</h1>';
-                        }
-                        ?>
+                        }  ?>
                     </a>
                 </div>
                 <div class=" nav__content  col-md-6">
@@ -50,13 +48,11 @@ if (!defined('ABSPATH')) {
                         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#collapsibleNavbar">
                             <span class="navbar-toggler-icon"></span>
                         </button>
-                        <div class="collapse navbar-collapse" id="collapsibleNavbar">
-                            <?php
+                        <div class="collapse navbar-collapse" id="collapsibleNavbar"> <?php
                             wp_nav_menu(array(
                                 'container'  => 'ul',
                                 'menu_class' => 'navbar-nav',
-                            ));
-                            ?>
+                            )); ?>
                         </div>
                     </nav>
                 </div>
