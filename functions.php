9d1d34eb5b65ccf9342e217c6f2c0a8c38f3edcb
Khemraj <khemraj@gmail.com>
2024-03-08 15:06:33 +0530

    Remove extra spaces in woocommerce support function

--- functions.php
+++ functions.php
@@ -7,10 +7,9 @@ require_once 'libs/helper.php';
 
 // add setup_woocommerce_support
 if(function_exists('setup_woocommerce_support')){
-  
+
   add_action( 'after_setup_theme', 'setup_woocommerce_support' );
-  function setup_woocommerce_support()
-  {
+  function setup_woocommerce_support() {
     add_theme_support('woocommerce');
   }
 }
