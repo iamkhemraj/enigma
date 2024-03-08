b19ab06de3e9ecb129ff1b2655feefa113997697
Khemraj <khemraj@gmail.com>
2024-03-08 17:46:57 +0530

    Remove image

--- template-parts/template-slider.php
+++ template-parts/template-slider.php
@@ -10,13 +10,13 @@ if (!defined('ABSPATH')) {
     <div class="splide__track">
         <ul class="splide__list">
             <li class="hmsplide__slide__img splide__slide">
-                <img src="<?= get_template_directory_uri().'/assets/img/Droogboeketten_kunstboeketten_kunstplanten_bestel_je_bij_Loviy_-_desktop_c36eea96-c825-49af-819e-fde5a1e1b24a_1400x662_crop_center.webp'?>" alt="">
+                <img src="<?= get_template_directory_uri().'/assets/img/hero/hero-banner-1.jpg'?>" alt="">
             </li>
             <li class="hmsplide__slide__img splide__slide">
-                <img src="<?= get_template_directory_uri().'/assets/img/VYPERLETE_PRO_1_3740x1266_crop_center.webp'?>" alt="">
+                <img src="<?= get_template_directory_uri().'/assets/img/hero/hero-banner-2.jpg'?>" alt="">
             </li>
             <li class="hmsplide__slide__img splide__slide">
-                <img src="<?= get_template_directory_uri().'/assets/img/istockphoto-513247652-1024x1024.jpg'?>" alt="">
+                <img src="<?= get_template_directory_uri().'/assets/img/hero/hero-banner-3.jpg'?>" alt="">
             </li>
         </ul>
     </div>
