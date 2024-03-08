473a64b0e062bf2e0d2bded589acc0d4a6c76e7a
Khemraj <khemraj@gmail.com>
2024-03-08 17:06:33 +0530

    Change home page slider  classes

--- template-parts/template-slider.php
+++ template-parts/template-slider.php
@@ -6,18 +6,21 @@ if (!defined('ABSPATH')) {
 // For hero slider...
 ?>
 
-<div id="main-slider" class="hero__slider hmsplide ">
-   <div class="hmsplide__track">
-      <ul class="hmsplide__list">
-         <li class="hmsplide__slide__img">
-            <img src="<?= get_template_directory_uri().'/assets/img/Droogboeketten_kunstboeketten_kunstplanten_bestel_je_bij_Loviy_-_desktop_c36eea96-c825-49af-819e-fde5a1e1b24a_1400x662_crop_center.webp'?>" alt="">
-         </li>
-         <li class="hmsplide__slide__img">
-            <img src="<?= get_template_directory_uri().'/assets/img/VYPERLETE_PRO_1_3740x1266_crop_center.webp'?>" alt="">
-         </li>
-         <li class="hmsplide__slide__img">
-            <img src="<?= get_template_directory_uri().'/assets/img/istockphoto-513247652-1024x1024.jpg'?>" alt="">
-         </li>
-		</ul>
-	</div>
+<div class="splide" id="main-slider">
+    <div class="splide__track">
+        <ul class="splide__list">
+            <li class="hmsplide__slide__img splide__slide">
+                <img src="<?= get_template_directory_uri().'/assets/img/Droogboeketten_kunstboeketten_kunstplanten_bestel_je_bij_Loviy_-_desktop_c36eea96-c825-49af-819e-fde5a1e1b24a_1400x662_crop_center.webp'?>" alt="">
+            </li>
+            <li class="hmsplide__slide__img splide__slide">
+                <img src="<?= get_template_directory_uri().'/assets/img/VYPERLETE_PRO_1_3740x1266_crop_center.webp'?>" alt="">
+            </li>
+            <li class="hmsplide__slide__img splide__slide">
+                <img src="<?= get_template_directory_uri().'/assets/img/istockphoto-513247652-1024x1024.jpg'?>" alt="">
+            </li>
+        </ul>
+    </div>
 </div>
+
+
+   
