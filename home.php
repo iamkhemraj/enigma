a92e4128c52943f06389d6739ffeaed65d6ef944
Khemraj <khemraj@gmail.com>
2024-03-08 16:20:37 +0530

    Colllection template part call on home page

--- home.php
+++ home.php
@@ -16,29 +16,12 @@ if ( ! defined('ABSPATH')){
 
 get_header();
 
-// Hero slider 
+/***
+ * Get templates part here.
+ */
 get_template_part( 'template-parts/template','slider' );
+get_template_part( 'template-parts/template','collection' );
+
+get_footer();
 
 ?>
-
-<!-- Main section close -->
-<section id="collection" class="collection">
-<div class="container">
-    <h2 class="section__title text-center p-2">New Collection</h2>
-    <div class="collection__content" id="collection__content">
-        <div class="container" style="background-color: rgb(255, 255, 255); padding: 16px;">
-            <div class="row">
-                <div class="col-md-12">
-                    <div class="product__content">
-                        <?php echo do_shortcode('[products limit="6" columns="6"]'); ?>
-                    </div>
-                    <div class="product__content">
-                        <?php echo do_shortcode('[products limit="6" columns="6"  order="desc"]'); ?>
-                    </div>
-                </div>
-            </div>
-        </div> 
-    </div>
-</div>
-</section>
-<?php get_footer(); ?>
