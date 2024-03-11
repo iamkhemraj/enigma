e33b51ab5cec871d00db1d07b3ca057f47f846db
Khemraj <khemraj@gmail.com>
2024-03-11 15:05:34 +0530

    add splide slider class in splide section li

--- template-parts/template-slider.php
+++ template-parts/template-slider.php
@@ -9,13 +9,13 @@ if (!defined('ABSPATH')) {
 <div class="splide" id="main-slider">
     <div class="splide__track">
         <ul class="splide__list">
-            <li class="hmsplide__slide__img splide__slide">
+            <li class="hmsplide__slide__img splide__slide" id ="splide__slider" >
                 <img src="<?= get_template_directory_uri().'/assets/img/hero/hero-banner-1.jpg'?>" alt="">
             </li>
-            <li class="hmsplide__slide__img splide__slide">
+            <li class="hmsplide__slide__img splide__slide" id ="splide__slider" >
                 <img src="<?= get_template_directory_uri().'/assets/img/hero/hero-banner-2.jpg'?>" alt="">
             </li>
-            <li class="hmsplide__slide__img splide__slide">
+            <li class="hmsplide__slide__img splide__slide" id ="splide__slider" >
                 <img src="<?= get_template_directory_uri().'/assets/img/hero/hero-banner-3.jpg'?>" alt="">
             </li>
         </ul>
