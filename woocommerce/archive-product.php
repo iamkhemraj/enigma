7fe5de3aad3fe03b414717e7e808115aeb5fd500
Khemraj <khemraj@gmail.com>
2024-03-12 16:05:04 +0530

    add html in archive page

--- woocommerce/archive-product.php
+++ woocommerce/archive-product.php
@@ -1,18 +1,50 @@
-<?php 
-if( !defined('ABSPATH') ){
-  exit; // Exit if accessce dierectly
-}
-
+<?php
 /**
  * 
- * The Template for displaying product archives, including the main shop page which is a post type archive
- * This template can be overridden by copying it to yourtheme/woocommerce/archive-product.php.
- *
+ * This is important file of this theme.
+ * It is archive page don't any thing without discuss senior.
+ * 
  */
-get_header(); 
+defined( 'ABSPATH' ) || exit;
 
-  echo "Archive page test";
+get_header( 'shop' );
+//  do_action( 'woocommerce_before_main_content' ); ?>
 
-get_footer(); 
 
-?>
+<div class="container">
+	<div class="row">
+        <div class="col-lg-3">  <?php
+		echo "side filteres";
+            ?>
+        </div>
+        <div class="col-lg-9">
+            <div class="woocommerce-products-header">
+                <?php if ( apply_filters( 'woocommerce_show_page_title', true ) ) : ?>
+                    <h3 class="woocommerce-products-header__title page-title"><?php woocommerce_page_title(); ?></h3>
+                <?php endif; ?>
+                <?php do_action( 'woocommerce_archive_description' ); ?>
+            </div>
+            <?php
+            if ( woocommerce_product_loop() ) {
+                do_action( 'woocommerce_before_shop_loop' );
+                woocommerce_product_loop_start();
+                if ( wc_get_loop_prop( 'total' ) ) {
+                    while ( have_posts() ) {
+                        the_post();
+                        do_action( 'woocommerce_shop_loop' );
+                        wc_get_template_part( 'content', 'product' );
+                    }
+                }
+                woocommerce_product_loop_end();
+                do_action( 'woocommerce_after_shop_loop' );
+            } else {
+                do_action( 'woocommerce_no_products_found' );
+            }
+            ?>
+        </div>
+    </div>
+</div> <?php
+
+do_action( 'woocommerce_after_main_content' );
+
+get_footer( 'shop' );
