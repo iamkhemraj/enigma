47b55c26c5b9b6da0f95fb0e6a55cc373519337a
Khemraj <khemraj@gmail.com>
2024-03-12 11:44:45 +0530

    Mantaine code according to our design

--- woocommerce/single-product.php
+++ woocommerce/single-product.php
@@ -1,44 +1,46 @@
 <?php
- if ( ! defined( 'ABSPATH' ) ) {
-	 exit; // Exit if accessed directly
- }
+if (!defined('ABSPATH')) {
+    exit; // Exit if accessed directly
+}
+
+get_header('shop');
 
 /**
- * 
- * This is important file don't delete this file. 
- * It is a single product page.
+ * woocommerce_before_main_content hook.
  *
+ * @hooked woocommerce_output_content_wrapper - 10 (outputs opening divs for the content)
+ * @hooked woocommerce_breadcrumb - 20
  */
+do_action('woocommerce_before_main_content');
+?>
+<div class="container">
+    <div class="row">
+        <div class="col-lg-8">
+            <?php
+            while (have_posts()) {
+                the_post();
+                wc_get_template_part('content', 'single-product');
+            }; // end of the loop. 
+            ?>
+        </div>
+        <div class="col-lg-4">
+            <?php
+            /**
+             * woocommerce_sidebar hook.
+             *
+             * @hooked woocommerce_get_sidebar - 10
+             */
+            do_action('woocommerce_sidebar');
+            ?>
+        </div>
+    </div>
+</div>
+<?php
+/**
+ * woocommerce_after_main_content hook.
+ *
+ * @hooked woocommerce_output_content_wrapper_end - 10 (outputs closing divs for the content)
+ */
+do_action('woocommerce_after_main_content');
 
-get_header( 'shop' ); 
-
-	/**
-	 * woocommerce_before_main_content hook.
-	 *
-	 * @hooked woocommerce_output_content_wrapper - 10 (outputs opening divs for the content)
-	 * @hooked woocommerce_breadcrumb - 20
-	 */
-	do_action( 'woocommerce_before_main_content');
-	while ( have_posts() ) {
-		the_post(); 
-		wc_get_template_part( 'content', 'single-product' ); 
-
-	}; // end of the loop. 
-
-	/**
-	 * woocommerce_after_main_content hook.
-	 *
-	 * @hooked woocommerce_output_content_wrapper_end - 10 (outputs closing divs for the content)
-	 */
-	do_action( 'woocommerce_after_main_content' );
-
-	/**
-	 * woocommerce_sidebar hook.
-	 *
-	 * @hooked woocommerce_get_sidebar - 10
-	 */
-	//do_action( 'woocommerce_sidebar' );
-	
-get_footer( 'shop' );
-
-/* Omit closing PHP tag at the end of PHP files to avoid "headers already sent" issues. */
+get_footer('shop');
